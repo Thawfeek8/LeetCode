bdf5800d0c45a5c176cff446d14410f637c1560e
THAWFEEK AHAMED <167204254+Thawfeek8@users.noreply.github.com>
2024-07-07 11:21:27 +0530

    Time: 0 ms (100.00%) | Memory: 40.5 MB (14.30%) - LeetSync

--- 1642-water-bottles/water-bottles.java
+++ 1642-water-bottles/water-bottles.java
@@ -1,12 +1,23 @@
+// class Solution {
+//     public int numWaterBottles(int numBottles, int numExchange) {
+//         int total = numBottles;
+
+//         while (numBottles >= numExchange) {
+//             total += numBottles / numExchange;
+//             numBottles = (numBottles / numExchange) + (numBottles % numExchange);
+//         }
+
+//         return total;
+//     }
+// }
 class Solution {
     public int numWaterBottles(int numBottles, int numExchange) {
-        int total = numBottles;
-
-        while (numBottles >= numExchange) {
-            total += numBottles / numExchange;
-            numBottles = (numBottles / numExchange) + (numBottles % numExchange);
+        int sum = numBottles;
+        int emp_count = numBottles;
+        while(emp_count/numExchange >=1){
+            sum += emp_count/numExchange;
+            emp_count = emp_count/numExchange + emp_count%numExchange;
         }
-
-        return total;
+        return sum;
     }
 }
