8946bafed8ac8774bc9fd0d0d809f424cab344c0
THAWFEEK AHAMED <167204254+Thawfeek8@users.noreply.github.com>
2024-07-26 17:54:22 +0530

    Time: 0 ms (100.00%) | Memory: 45.5 MB (16.32%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -12,3 +12,20 @@ class Solution {
         return ans^anss;
     }
 }
+
+// class Solution {
+//     public int missingNumber(int[] nums) {
+//         int[] nums1 = new int[nums.length+1];
+//         int ans = 0;
+//         Arrays.fill(nums1,-1);
+//         for(int i=0;i<nums.length;i++){
+//             nums1[nums[i]] = nums[i];
+//         }
+//         for(int i=0;i<nums1.length;i++){
+//             if(nums1[i] == -1){
+//                 ans = i;
+//             }
+//         }
+//         return ans;
+//     }
+// }
