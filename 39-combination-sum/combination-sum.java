d1306b866c60845a4decf81ad3b6e22532aa36e4
THAWFEEK AHAMED <167204254+Thawfeek8@users.noreply.github.com>
2024-08-07 14:32:01 +0530

    Time: 2 ms (84.29%) | Memory: 44.5 MB (46.40%) - LeetSync

---
+++ 39-combination-sum/combination-sum.java
@@ -0,0 +1,27 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> ds = new ArrayList<>();
+        findcombination(0,candidates,ds,target,res);
+
+        return res;
+    }
+
+    public static void findcombination(int i,int[] arr,List<Integer> ds
+    ,int sum,List<List<Integer>> res){
+        if(i == arr.length){
+            if(sum == 0){ 
+                res.add(new ArrayList<>(ds));
+            }
+            return;
+        }
+            //pick
+            if(arr[i] <= sum){
+                ds.add(arr[i]);
+                findcombination(i,arr,ds,sum-arr[i],res);
+                ds.remove(ds.size()-1);
+            }
+            //not pick
+            findcombination(i+1,arr,ds,sum,res);
+    } 
+}
