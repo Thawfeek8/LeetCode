e7d21f500ad09e7a569253cffadbdac7dc1427a1
THAWFEEK AHAMED <167204254+Thawfeek8@users.noreply.github.com>
2024-08-18 12:48:22 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (24.50%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,21 +1,23 @@
 class Solution {
     public int climbStairs(int n) {
         int[] dp = new int[n+1];
-        Arrays.fill(dp, -1);
-        return ducati(n, dp);
-    }
-    static int ducati(int n, int[] dp){
-        if(n==0||n==1) return 1;
-        if(dp[n]!=-1) return dp[n];
-        int a = ducati(n-1,dp);
-        int b = ducati(n-2,dp);
-        dp[n] = a+b;
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i] = dp[i-2]+dp[i-1];
+        }
         return dp[n];
-
-
-
-
-
-        
     }
+    // static int ducati(int n, int[] dp){
+    //     if(n==0||n==1) return 1;
+
+    //     if(dp[n]!=-1) return dp[n];
+
+    //     int a = ducati(n-1,dp);
+    //     int b = ducati(n-2,dp);
+    //     dp[n] = a+b;
+        
+    //     return dp[n];
+    // }
 }
+
