e0779bbb4d514291bda1ccbb95cc9c2ffabf1d7a
THAWFEEK AHAMED <167204254+Thawfeek8@users.noreply.github.com>
2024-08-22 18:00:45 +0530

    Time: 1 ms (100.00%) | Memory: 42.3 MB (5.62%) - LeetSync

--- 389-find-the-difference/find-the-difference.java
+++ 389-find-the-difference/find-the-difference.java
@@ -2,10 +2,10 @@ class Solution {
     public char findTheDifference(String s, String t) {
         int sum=0;
         for(int ch: t.toCharArray()){
-            sum +=ch;
+            sum ^=ch;
         }
         for(int ch: s.toCharArray()){
-            sum -= ch;
+            sum ^= ch;
         }
         return (char)(sum);
     }
