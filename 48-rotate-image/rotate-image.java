c0601a873e71bd4acdd4f35f961139f391296e8b
THAWFEEK AHAMED <167204254+Thawfeek8@users.noreply.github.com>
2024-11-23 17:15:43 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (28.61%) - LeetSync

---
+++ 48-rotate-image/rotate-image.java
@@ -0,0 +1,26 @@
+class Solution {
+    public void rotate(int[][] mat) {
+        int n = mat.length;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                int temp = mat[i][j];
+                mat[i][j] = mat[j][i];
+                mat[j][i] = temp;
+            }
+        }
+        for(int i=0;i<n;i++){
+            reverse(mat[i]);
+        }
+    }
+    static void reverse(int[] arr){
+        int i = 0;
+        int j = arr.length-1;
+        while(i<j){
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+    }
+}
